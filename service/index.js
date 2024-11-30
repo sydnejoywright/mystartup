9e8a69e46d2bc3c82b89ae9104eef00970a5ef34
sydnejoywright <sydnebrilliant@gmail.com>
2024-11-30 16:29:12 -0700

    adding the ports to the index.js file

--- service/index.js
+++ service/index.js
@@ -1,3 +1,19 @@
+const express = require('express');
+const app = express();
+
+
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.get('*', (_req, res) => {
+  res.send({ msg: 'mystartup service' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+
+
 
 app.use(express.static('public'));
